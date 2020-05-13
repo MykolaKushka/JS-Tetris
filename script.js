cca3bced48c0585fcab9b6ad6c0d97f586fe56e0
Mykola Kushka <Mykola.Kushka@gmail.com>
2020-05-13 10:31:47 +0300

    Draw initial play field

---
+++ script.js
@@ -0,0 +1,45 @@
+let main = document.querySelector(".main");
+
+let playfield = [
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 0, 0, 0, 0],
+  ];
+
+  // Draw play field
+  const draw = () => {
+    let mainInnerHTML = "";
+
+    for (let y in playfield) {
+        for (let x in playfield[y]) {
+            if (playfield[y][x] === 1) {
+                mainInnerHTML += '<div class="cell movingCell"></div>';
+              } else if (playfield[y][x] === 2) {
+                mainInnerHTML += '<div class="cell fixedCell"></div>';
+              } else {
+                mainInnerHTML += '<div class="cell"></div>';
+              }
+        }
+    }
+
+    main.innerHTML = mainInnerHTML;
+  }
+
+  draw();
