5d1020e0083b9b76a999ca81718f2a8e24b76030
Mykola Kushka <Mykola.Kushka@gmail.com>
2020-05-13 15:23:30 +0300

    Move tetros to left and right

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let main = document.querySelector(".main");
 
 let playfield = [
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -29,8 +29,8 @@ let playfield = [
   const draw = () => {
     let mainInnerHTML = "";
 
-    for (let y in playfield) {
-        for (let x in playfield[y]) {
+    for (let y = 0; y < playfield.length; y++) {
+        for (let x = 0; x < playfield[y].length; x++) {
             if (playfield[y][x] === 1) {
                 mainInnerHTML += '<div class="cell movingCell"></div>';
               } else if (playfield[y][x] === 2) {
@@ -45,10 +45,10 @@ let playfield = [
   }
 
 
-  // Move tetro down
+  // Move tetro to the down
   const canTetroMoveDown = () => {
     for (let y = 0; y < playfield.length; y++) {
-        for (let x in playfield[y]) {
+        for (let x = 0; x < playfield[y].length; x++) {
         if (playfield[y][x] === 1) {
           if (y === playfield.length - 1 || playfield[y + 1][x] === 2) {
             return false;
@@ -63,7 +63,7 @@ let playfield = [
   const moveTetroDown = () => {
     if (canTetroMoveDown()) {
         for (let y = playfield.length - 1; y >= 0; y--) {
-        for (let x in playfield[y]) {
+        for (let x = 0; x < playfield[y].length; x++) {
           if (playfield[y][x] === 1) {
             playfield[y + 1][x] = 1;
             playfield[y][x] = 0;
@@ -75,21 +75,94 @@ let playfield = [
     }
   }
 
-  // Fix tetro
-  const fixTetro = () => {
-    for (let y in playfield) {
-        for (let x in playfield[y]) {
+
+// Move tetro to the left
+const canTetroMoveLeft = () => {
+    for (let y = 0; y < playfield.length; y++) {
+      for (let x = 0; x < playfield[y].length; x++) {
         if (playfield[y][x] === 1) {
-          playfield[y][x] = 2;
+          if (x === 0 || playfield[y][x - 1] === 2) {
+            return false;
+          }
+        }
+      }
+    }
+  
+    return true;
+  }
+  
+  const moveTetroLeft = () => {
+    if (canTetroMoveLeft()) {
+      for (let y = playfield.length - 1; y >= 0; y--) {
+        for (let x = 0; x < playfield[y].length; x++) {
+          if (playfield[y][x] === 1) {
+            playfield[y][x - 1] = 1;
+            playfield[y][x] = 0;
+          }
         }
       }
     }
   }
 
 
+// Move tetro to the Right
+const canTetroMoveRight = () => {
+  for (let y = 0; y < playfield.length; y++) {
+    for (let x = 0; x < playfield[y].length; x++) {
+      if (playfield[y][x] === 1) {
+        if (x === 9 || playfield[y][x + 1] === 2) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return true;
+}
+
+const moveTetroRight = () => {
+  if (canTetroMoveRight()) {
+    for (let y = playfield.length - 1; y >= 0; y--) {
+      for (let x = 9; x >= 0; x--) {
+        if (playfield[y][x] === 1) {
+          playfield[y][x + 1] = 1;
+          playfield[y][x] = 0;
+        }
+      }
+    }
+  }
+}
+
+
+
+  // Fix tetro
+  const fixTetro = () => {
+    for (let y = 0; y < playfield.length; y++) {
+        for (let x = 0; x < playfield[y].length; x++) {
+        if (playfield[y][x] === 1) {
+          playfield[y][x] = 2;
+        }
+      }
+    }
+
+    playfield[0] = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0];
+    playfield[1] = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0];
+  }
+
+  document.onkeydown = (e) => {
+    if (e.keyCode === 37) {
+      moveTetroLeft();
+    } else if (e.keyCode === 39) {
+      moveTetroRight();
+    } else if (e.keyCode === 40) {
+      moveTetroDown();
+    }
+    draw();
+  };
+
   draw();
   
-  function startGame() {
+  const startGame = () => {
     moveTetroDown();
     draw();
     setTimeout(startGame, gameSpeed);
